322f2dedf01d27992086a35c7505e2983a5187be
Varshitha2925 <57656739+Varshitha2925@users.noreply.github.com>
2020-03-18 18:03:29 +0530

    Create Word.py

---
+++ Word.py
@@ -0,0 +1,16 @@
+
+s = str(input())
+u = 0
+l = 0
+for i in s:
+  if i.isupper():
+    u += 1
+    
+  if i.islower():
+    
+    l += 1
+
+if u > l:
+  print(s.upper())
+else:
+  print(s.lower())       
